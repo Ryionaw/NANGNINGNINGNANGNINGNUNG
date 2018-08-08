a60f6e5eb18310861c09daffeefa76b93c65be0d
Ryionaw <42166522+Ryionaw@users.noreply.github.com>
2018-08-08 11:27:21 +0700

    Add files via upload

--- helo.js
+++ helo.js
@@ -1,1 +1,54 @@
-ASEK SAEK
+// Soal//
+// [Angka]//
+// [5,-6,-8,4,-3,6]//
+// positif 15 negatif -17 //
+// Diminta://
+// negative win // draw //
+
+// // SOAL 1  YAY // //
+// // OM TELOLET OM // //
+// var array = [9,-9]
+//     positive = array.filter(function (a) { return a >= 0; }),
+//     sum1 = positive.reduce(function (a, b) { return a + b; });
+//     negative = array.filter(function (a) { return a < 0; }),
+//     sum2 = Math.abs(negative.reduce(function (a, b) { return a + b; }));
+    
+// if (sum1>sum2)
+// {
+//     console.log('Positive Wins')
+// }
+// else if (sum2>sum1)
+// {
+//     console.log('Negative Wins')
+// }
+// else if (sum2===sum1)
+// {
+//     console.log('DRAW')
+// }
+
+
+
+
+
+// Soal 2 MPRET //
+// String 'A%6&z#7c'
+// Outputnya harus : Angka [6,7]
+//                 : Huruf [A,Z,C]
+// REGEXP = ('([0-9]+)|([a-zA-Z]+)','g')
+
+
+// var soal = 'A%6&z#7c'
+// var rumus = /[^A-Za-z]/g
+// var huruf = soal.replace(rumus, '')
+// var rumus2 = /[^0-9]/g
+// var angka = soal.replace(rumus2, '')
+
+// var ahkirnyajadi = huruf.toUpperCase().split("")
+// console.log(ahkirnyajadi)
+
+// ahkirnyakelar = angka.split('')
+
+// console.log(ahkirnyakelar)
+
+
+
